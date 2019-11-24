f74511837f395c15a43cea9634ac3630f2da66a8
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-11-24 12:51:16 +0100

    refactored index

--- Proyectos/Proyecto 1 - LifeSite/index.js
+++ Proyectos/Proyecto 1 - LifeSite/index.js
@@ -7,16 +7,17 @@ const path = require('path');
 
 const app = Express();
 
-// TODO Middlewares
-app.use('/users', userRouter); // Applying usersRouter to the main route
-app.use('/questions', questionsRouter); // Applying questions to the main route
+/* MIDDLEWARES */
 app.use(Express.static(path.join(__dirname, 'public')));
 
-//Load templates
+/* ROUTERS */
+app.use('/users', userRouter); // Applying usersRouter to the main route
+app.use('/questions', questionsRouter); // Applying questions to the main route
+
+
+/* LOAD TEMPLATES */
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
-
-app.listen(3000, (err) => console.log(err ? `[ERROR] ${err.message}` : '[INFO] Listening to port 3000!'));
 
 app.get('/', (request, response) => {
     response.redirect('/users/login');
@@ -33,3 +34,5 @@ app.use(function (err, request, response, next) { //  500: Internal server error
         message: err.message,
     });
 });
+
+app.listen(3000, (err) => console.log(err ? `[ERROR] ${err.message}` : '[INFO] Listening to port 3000!'));
